ad44a9089441265e714f894c51e642fd64727247
Blair Mason <rbmj@verizon.net>
2020-05-15 12:42:59 -0400

    Changes for MSVC compatibility

--- range_utils_common.h
+++ range_utils_common.h
@@ -1,8 +1,21 @@
 #ifndef RANGE_UTILS_COMMON_H_INC
 #define RANGE_UTILS_COMMON_H_INC
 
-template <class T> struct remove_rvalue_ref { typedef T type; };
-template <class T> struct remove_rvalue_ref<T&> { typedef T& type; };
-template <class T> struct remove_rvalue_ref<T&&> { typedef T type; };
+#ifdef RANGE_UTILS_USE_RANGEv3
+#include <range/v3/range/concepts.hpp>
+namespace range_utils {
+	namespace ranges = ::ranges;
+}
+#else
+#include <ranges>
+namespace range_util {
+	namespace ranges = std::ranges;
+}
+#endif
 
+namespace range_utils {
+	template <class T> struct remove_rvalue_ref { typedef T type; };
+	template <class T> struct remove_rvalue_ref<T&> { typedef T& type; };
+	template <class T> struct remove_rvalue_ref<T&&> { typedef T type; };
+}
 #endif
